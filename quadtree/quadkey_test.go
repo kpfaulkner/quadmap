cb079031d7868e6079de6240ab8deccb74fea27d
David McLeish <david.mcleish@nearmap.com>
2023-02-17 09:48:02 +1100

    make QuadKey a type

--- quadtree/quadkey_test.go
+++ quadtree/quadkey_test.go
@@ -9,29 +9,28 @@ import (
 const (
 
 	// levels 1-6 are populated (first 12 bits) and can see level (6) indicated at end of binary
-	QuadKey uint64 = 0b1101110110110000000000000000000000000000000000000000000000000110
+	quadKey QuadKey = 0b1101110110110000000000000000000000000000000000000000000000000110
 
 	// children of QuadKey. Note bits 12 and 13 as well as zoom at end.
-	Child0 uint64 = 0b1101110110110000000000000000000000000000000000000000000000000111
-	Child1 uint64 = 0b1101110110110100000000000000000000000000000000000000000000000111
-	Child2 uint64 = 0b1101110110111000000000000000000000000000000000000000000000000111
-	Child3 uint64 = 0b1101110110111100000000000000000000000000000000000000000000000111
+	Child0 QuadKey = 0b1101110110110000000000000000000000000000000000000000000000000111
+	Child1 QuadKey = 0b1101110110110100000000000000000000000000000000000000000000000111
+	Child2 QuadKey = 0b1101110110111000000000000000000000000000000000000000000000000111
+	Child3 QuadKey = 0b1101110110111100000000000000000000000000000000000000000000000111
 
 	// Parent is same as Quadkey but bits 10-11 are zeroed and length (at end of binary) now reads 5
-	ParentQuadKey uint64 = 0b1101110110000000000000000000000000000000000000000000000000000101
+	parent QuadKey = 0b1101110110000000000000000000000000000000000000000000000000000101
 )
 
 // TestGetParentQuadKey checks parent calculation is correct
 func TestGetParentQuadKey(t *testing.T) {
 
 	// confirm we've got zoom 6
-	zoom := GetTileZoomLevel(QuadKey)
-	assert.Equal(t, uint8(6), zoom, "Zoom level should be 6")
+	assert.Equal(t, uint8(6), quadKey.Zoom(), "Zoom level should be 6")
 
-	parentQuadKey, err := GetParentQuadKey(QuadKey)
+	parentQuadKey, err := quadKey.Parent()
 	assert.Nil(t, err, "Should not have error when getting parent quadkey")
-	assert.Equal(t, ParentQuadKey, parentQuadKey, "Parent quadkey incorrect")
-	assert.Equal(t, uint8(5), GetTileZoomLevel(parentQuadKey), "Parent zoom level should be 5")
+	assert.Equal(t, parent, parentQuadKey, "Parent quadkey incorrect")
+	assert.Equal(t, uint8(5), parentQuadKey.Zoom(), "Parent zoom level should be 5")
 
 }
 
@@ -41,27 +40,26 @@ func TestGetParentQuadKey(t *testing.T) {
 func TestGetChildQuadKeyForPos(t *testing.T) {
 
 	// confirm we've got zoom 6
-	zoom := GetTileZoomLevel(QuadKey)
-	assert.Equal(t, uint8(6), zoom, "Zoom level should be 6")
+	assert.Equal(t, uint8(6), quadKey.Zoom(), "Zoom level should be 6")
 
-	childPos0, err := GetChildQuadKeyForPos(QuadKey, 0)
+	childPos0, err := quadKey.ChildAtPos(0)
 	assert.Nil(t, err, "Should not have error when getting child quadkey")
 	assert.Equal(t, Child0, childPos0, "Child quadkey incorrect")
-	assert.Equal(t, uint8(7), GetTileZoomLevel(childPos0), "Child zoom level should be 7")
+	assert.Equal(t, uint8(7), childPos0.Zoom(), "Child zoom level should be 7")
 
-	childPos1, err := GetChildQuadKeyForPos(QuadKey, 1)
+	childPos1, err := quadKey.ChildAtPos(1)
 	assert.Nil(t, err, "Should not have error when getting child quadkey")
 	assert.Equal(t, Child1, childPos1, "Child quadkey incorrect")
-	assert.Equal(t, uint8(7), GetTileZoomLevel(childPos1), "Child zoom level should be 7")
+	assert.Equal(t, uint8(7), childPos1.Zoom(), "Child zoom level should be 7")
 
-	childPos2, err := GetChildQuadKeyForPos(QuadKey, 2)
+	childPos2, err := quadKey.ChildAtPos(2)
 	assert.Nil(t, err, "Should not have error when getting child quadkey")
 	assert.Equal(t, Child2, childPos2, "Child quadkey incorrect")
-	assert.Equal(t, uint8(7), GetTileZoomLevel(childPos2), "Child zoom level should be 7")
+	assert.Equal(t, uint8(7), childPos2.Zoom(), "Child zoom level should be 7")
 
-	childPos3, err := GetChildQuadKeyForPos(QuadKey, 3)
+	childPos3, err := quadKey.ChildAtPos(3)
 	assert.Nil(t, err, "Should not have error when getting child quadkey")
 	assert.Equal(t, Child3, childPos3, "Child quadkey incorrect")
-	assert.Equal(t, uint8(7), GetTileZoomLevel(childPos3), "Child zoom level should be 7")
+	assert.Equal(t, uint8(7), childPos3.Zoom(), "Child zoom level should be 7")
 
 }
